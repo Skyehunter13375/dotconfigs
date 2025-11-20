aae36c6d30b26388768704aebe508c7fa6ac9ab1
Patrick Kelley <kelley13375p@gmail.com>
2025-11-20 10:22:20 -0500

    Should be final version of initial setup script

--- SetupScripts/newSetup.sh
+++ SetupScripts/newSetup.sh
@@ -16,7 +16,7 @@ setuplg="${projDir}/setup.log"
 
 
 echo '┣━━━━━━━━━━━━━━━━━━┫ Installing RPMFusion (Free + Non-Free) ┣━━━━━━━━━━━━━━━━━━┫'
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
 
@@ -33,19 +33,32 @@ sudo snap install obsidian --classic
 
 
 echo '┣━━━━━━━━━━━━━━━━━━━━━━━━━┫ Grabbing my github repos ┣━━━━━━━━━━━━━━━━━━━━━━━━━┫'
-git clone https://github.com/Skyehunter13375/NullSky-Nvim.git        ${projDir}/nvim
+git clone https://github.com/Skyehunter13375/NullSky-Nvim.git ${projDir}/nvim
 git clone https://github.com/Skyehunter13375/Spacetraders_Client.git ${projDir}/SpaceTraders
 
 
 echo '┣━━━━━━━━━━━━━━━━━━━━━━━━━┫ Creating config symlinks ┣━━━━━━━━━━━━━━━━━━━━━━━━━┫'
-ln -sf ${confDir}/.config/nvim          ~/.config/nvim;          ls -la ~/.config/nvim
-ln -sf ${confDir}/.config/kitty         ~/.config/kitty;         ls -la ~/.config/kitty
-ln -sf ${confDir}/.config/starship.toml ~/.config/starship.toml; ls -la ~/.config/starship.toml
-ln -sf ${confDir}/.tmux.conf            ~/.tmux.conf;            ls -la ~/.tmux.conf
-ln -sf ${confDir}/.bashrc               ~/.bashrc;               ls -la ~/.bashrc
+ln -sf ${confDir}/.config/nvim ~/.config/nvim
+ls -la ~/.config/nvim
+
+ln -sf ${confDir}/.config/kitty ~/.config/kitty
+ls -la ~/.config/kitty
+
+ln -sf ${confDir}/.config/starship.toml ~/.config/starship.toml
+ls -la ~/.config/starship.toml
+
+ln -sf ${confDir}/.tmux.conf ~/.tmux.conf
+ls -la ~/.tmux.conf
+
+ln -sf ${confDir}/.bashrc ~/.bashrc
+ls -la ~/.bashrc
+
 
 echo '┣━━━━━━━━━━━━━━━━━━━━━━━┫ Linking root configs to mine ┣━━━━━━━━━━━━━━━━━━━━━━━┫'
 sudo mkdir /root/.config
-sudo ln -sf "$HOME/.config/nvim" /root/.config/nvim; ls -la ~/root/.config/nvim
-sudo ln -sf "$HOME/.bashrc"      /root/.bashrc;      ls -la ~/root/.bashrc
+sudo ln -sf "$HOME/.config/nvim" /root/.config/nvim
+ls -la /root/.config/nvim
+
+sudo ln -sf "$HOME/.bashrc" /root/.bashrc
+ls -la /root/.bashrc
 
