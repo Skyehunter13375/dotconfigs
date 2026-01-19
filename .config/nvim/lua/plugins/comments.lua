754a49ca8813ef3e4bb768d449bf18a07e52c0f5
Patrick K. <kelley13375p@gmail.com>
2026-01-19 12:59:20 -0500

    Modified comment styling slightly

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -1,4 +1,13 @@
 return {
+  -- # BUGS This is a test
+  -- # DEBG This is a test
+  -- # TODO This is a test
+  -- # WARN This is a test
+  -- # FEAT This is a test
+  -- # TASK This is a test
+  -- # TEST This is a test
+  -- # NOTE This is a test
+
   "folke/todo-comments.nvim",
   event = "VimEnter",
   dependencies = { "nvim-lua/plenary.nvim" },
@@ -6,17 +15,14 @@ return {
       signs = true,
       sign_priority = 8,
       keywords = {
-        BUGS = { icon = " ", color = "#da3633" },
-        HACK = { icon = " ", color = "#FF00FF" },
-        DEBG = { icon = "? ", color = "#FF00FF" },
-        TODO = { icon = " ", color = "#d29922" },
-        WARN = { icon = " ", color = "#d29922" },
-        FEAT = { icon = "+ ", color = "#d29922" },
-        TASK = { icon = " ", color = "#3fb950" },
-        TEST = { icon = "⏲ ", color = "#3fb950" },
-        PERF = { icon = " ", color = "#3fb950" },
-        NOTE = { icon = " ", color = "#58a6ff" },
-        INFO = { icon = "! ", color = "#58a6ff" },
+        BUGS = { icon = " ", color = "#C97A7A" },
+        DEBG = { icon = "? ", color = "#C06A9A" },
+        TODO = { icon = "⨯ ", color = "#A37D3E" },
+        WARN = { icon = " ", color = "#BFA23A" },
+        FEAT = { icon = "+ ", color = "#7FA65A" },
+        TASK = { icon = " ", color = "#4FA38A" },
+        TEST = { icon = "⏲ ", color = "#4F8FB3" },
+        NOTE = { icon = "! ", color = "#7E6BB3" },
       },
       gui_style = {
         fg = "NONE",
@@ -28,9 +34,9 @@ return {
         multiline_pattern = "^.",
         multiline_context = 10,
         before            = "fg",      -- "fg" or "bg" or empty
-        keyword           = "wide_bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty
+        keyword           = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty
         after             = "fg",      -- "fg" or "bg" or empty
-        pattern           = [[.*(KEYWORDS)\s*:]],
+        pattern           = [[.*(KEYWORDS)\s* ]],
         comments_only     = true,
         max_line_len      = 400,
         exclude           = {},
